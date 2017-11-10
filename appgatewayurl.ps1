b563002c4d29dec28f3662a8b8c8eba7c57b070c
David Murray <davidmu@microsoft.com>
2017-11-09 16:03:17 -0800

    Update appgatewayurl.ps1

--- appgatewayurl.ps1
+++ appgatewayurl.ps1
@@ -1,8 +1,3 @@
 Add-WindowsFeature Web-Server
 Add-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value $($env:computername)
-mkdir "C:\inetpub\wwwroot\images"
-$imagevalue = "Images: " + $($env:computername)
-Add-Content -Path "C:\inetpub\wwwroot\images\test.htm" -Value $imagevalue
-mkdir "C:\inetpub\wwwroot\video"
-$videovalue = "Video: " + $($env:computername)
-Add-Content -Path "C:\inetpub\wwwroot\video\test.htm" -Value $videovalue
+
