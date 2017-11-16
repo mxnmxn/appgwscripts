3edbde8b192376a7591bd57ac438713b3659af4b
David Murray <davidmu@microsoft.com>
2017-11-16 13:10:35 -0800

    Update install_nginx.sh

--- install_nginx.sh
+++ install_nginx.sh
@@ -1,6 +1,4 @@
 apt-get install -y nginx
-sh -c 'echo $HOSTNAME > /var/www/html/index.html'
-sh -c 'mkdir -p /var/www/html/images'
-sh -c 'echo "Images: " $HOSTNAME > /var/www/html/images/test.html'
-sh -c 'mkdir -p /var/www/html/video'
-sh -c 'echo "Video: " $HOSTNAME > /var/www/html/video/test.html'
+echo $HOSTNAME | sudo tee -a /var/www/html/index.html
+echo "Images: " $HOSTNAME | sudo tee -a /var/www/html/images/test.html
+echo "Video: " $HOSTNAME | sudo tee -a /var/www/html/video/test.html
