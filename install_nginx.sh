724c1357f54161fa5e13eceb228dc590750a7d9c
mxnmxn <mxnmxn@gmail.com>
2022-08-02 21:53:15 +0300

    Update install_nginx.sh

--- install_nginx.sh
+++ install_nginx.sh
@@ -1,5 +1,5 @@
 apt-get install -y nginx
-echo $(hostname) | sudo tee /var/www/html/index.html
+echo "This page is served by vmss instance " $(hostname) | sudo tee /var/www/html/index.html
 sudo mkdir -p /var/www/html/images
 echo "Images: " $(hostname) | sudo tee /var/www/html/images/test.html
 sudo mkdir -p /var/www/html/video
